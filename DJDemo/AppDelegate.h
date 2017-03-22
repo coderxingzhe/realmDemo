29bb4d919c7fb1b5a2ff26007b988669c618e01b
段自强 <you@example.com>
2017-03-22 10:54:53 +0800

    Initial Commit

---
+++ DJDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJDemo
+//
+//  Created by 段自强 on 2017/3/22.
+//  Copyright © 2017年 段自强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJDemo
+//
+//  Created by 段自强 on 2017/3/22.
+//  Copyright © 2017年 段自强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
